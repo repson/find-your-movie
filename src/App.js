2acbfcdf3b8fd6b8de491386f17793d7a1b21971
Isaac Peña <ipena@wtelecom.es>
2019-05-01 18:14:15 +0200

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Hello (props) {
+  return <h2>{props.title}</h2>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Hello title='Hello from props'/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
